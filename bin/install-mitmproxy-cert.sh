e3c678ec764bd27b641422c92dd57936f46b0101
Nathan Jones <nathan@ncjones.com>
2016-01-20 21:36:11 +1300

    add java cacerts to mitmproxy install script

--- bin/install-mitmproxy-cert.sh
+++ bin/install-mitmproxy-cert.sh
@@ -1,3 +1,14 @@
 #!/bin/sh
-sudo ln -s ~/.mitmproxy/mitmproxy-ca-cert.cer /usr/lib/ssl/certs/mitmproxy_ca_cert.pem
+mitmproxy_ca_cert=$HOME/.mitmproxy/mitmproxy-ca-cert.cer
+java_bin_dir=$(dirname $(readlink -f $(which java)))
+jre_home=$(cd "$java_bin_dir/.." && pwd)
+java_keystore=$jre_home/lib/security/cacerts
+sudo keytool -import \
+  -trustcacerts \
+  -noprompt \
+  -storepass changeit \
+  -alias mitmproxy \
+  -file $mitmproxy_ca_cert \
+  -keystore $java_keystore
+sudo ln -s $mitmproxy_ca_cert /usr/lib/ssl/certs/mitmproxy_ca_cert.pem
 sudo c_rehash /usr/lib/ssl/certs
