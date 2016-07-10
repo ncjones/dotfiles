c4f2be3dfa119de8ed5874c7716013e6a175b011
Nathan Jones <nathan@ncjones.com>
2016-07-11 10:06:27 +1200

    Allow mitmproxy cert to install without Java

--- bin/install-mitmproxy-cert.sh
+++ bin/install-mitmproxy-cert.sh
@@ -1,14 +1,23 @@
 #!/bin/sh
+
+install_system_cert() {
+  sudo ln -s "$mitmproxy_ca_cert" /usr/lib/ssl/certs/mitmproxy_ca_cert.pem
+  sudo c_rehash /usr/lib/ssl/certs
+}
+
+install_java_cert() {
+  java_bin_dir="$(dirname "$(readlink -f "$(which java)")")"
+  jre_home=$(cd "$java_bin_dir/.." && pwd)
+  java_keystore=$jre_home/lib/security/cacerts
+  sudo keytool -import \
+    -trustcacerts \
+    -noprompt \
+    -storepass changeit \
+    -alias mitmproxy \
+    -file "$mitmproxy_ca_cert" \
+    -keystore "$java_keystore"
+}
+
 mitmproxy_ca_cert=$HOME/.mitmproxy/mitmproxy-ca-cert.cer
-java_bin_dir=$(dirname $(readlink -f $(which java)))
-jre_home=$(cd "$java_bin_dir/.." && pwd)
-java_keystore=$jre_home/lib/security/cacerts
-sudo keytool -import \
-  -trustcacerts \
-  -noprompt \
-  -storepass changeit \
-  -alias mitmproxy \
-  -file $mitmproxy_ca_cert \
-  -keystore $java_keystore
-sudo ln -s $mitmproxy_ca_cert /usr/lib/ssl/certs/mitmproxy_ca_cert.pem
-sudo c_rehash /usr/lib/ssl/certs
+install_system_cert
+which java && install_java_cert
