234fff507b524dd2a73cf7123e12b719f0de71d3
Nathan Jones <nathan@ncjones.com>
2020-05-01 12:12:54 +1200

    Avoid writing empty vim plug file

--- vim/install.sh
+++ vim/install.sh
@@ -8,6 +8,5 @@ vim_plug_file=$basedir/.vim/autoload/plug.vim
 if [ ! -f $vim_plug_file ]; then
     echo downloading $vim_plug_url
     mkdir -p $(dirname $vim_plug_file)
-    wget --quiet -O - $vim_plug_url > $vim_plug_file
+    wget --quiet -O $vim_plug_file $vim_plug_url
 fi
-
