56bea659f2c3c029be62d2fdbbe312ef3559ad99
Nathan Jones <nathan@ncjones.com>
2026-04-10 19:47:02 +1200

    Add lazyvim yank lsp message keymap

--- lazyvim/.nvim/lua/config/keymaps.lua
+++ lazyvim/.nvim/lua/config/keymaps.lua
@@ -1,3 +1,11 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set("n", "<leader>cy", function()
+  local diags = vim.diagnostic.get(0, { lnum = vim.fn.line(".") - 1 })
+  if #diags > 0 then
+    vim.fn.setreg("+", diags[1].message)
+    vim.notify("Copied: " .. diags[1].message)
+  end
+end, { desc = "Copy Diagnostic Message" })
